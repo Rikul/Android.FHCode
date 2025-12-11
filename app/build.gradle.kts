e918e5e9e49f0c4d7dd8ee8c93c9a168cc971eba
Rikul patel <rikulpatel@gmail.com>
2025-12-10 19:39:26 -0600

    added test deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,14 @@ dependencies {
     androidTestImplementation("androidx.test:core:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("org.robolectric:robolectric:4.11.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
 
 configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
